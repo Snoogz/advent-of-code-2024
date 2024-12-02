0a3ad024aad5e21a83f50ffc6a02917834a29ad0
Nathan Ramirez <nathan.ramirez@alkami.com>
2024-12-02 11:45:58 -0600

    init

---
+++ Day1/Day1Logic.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode2024.Day1;
+
+public class Day1Logic
+{
+    private readonly string[] _input = File.ReadAllLines("Day1/input.txt");
+    private List<long> _leftList = [];
+    private List<long> _rightList = [];
+
+    public async Task ParseInputIntoLists()
+    {
+        foreach (string line in _input)
+        {
+            string[] splitLine = line.Split(' ');
+            
+            long leftValue = long.Parse(splitLine[0]);
+            long rightValue = long.Parse(splitLine[3]);
+            
+            await SortValueIntoList(_leftList, leftValue);
+            await SortValueIntoList(_rightList, rightValue);
+        }
+        
+        // Console.WriteLine($"Left list: {string.Join(',', leftList)}");
+        // Console.WriteLine($"Right list: {string.Join(',', rightList)}");
+    }
+    
+    public Task<long> GetTotalDistance()
+    {
+         return Task.FromResult(_leftList.Select((t, i) => Math.Abs(t - _rightList[i])).Sum());
+    }
+
+    public Task<long> GetSimilarityScore()
+    {
+        long similarityScore = 0; 
+        foreach (long leftValue in _leftList)
+        {
+            int occurence = _rightList.Count(rl => rl.Equals(leftValue));
+            // Console.WriteLine($"[{leftValue}] occured [{occurence}] times in right list");
+            similarityScore += leftValue * occurence;
+        }
+        return Task.FromResult(similarityScore);
+    }
+
+    private static Task SortValueIntoList(List<long> list, long value)
+    {
+        List<long> copyList = list.ToList();
+        if (copyList.Count == 0)
+        {
+            list.Add(value);
+            return Task.CompletedTask;
+        }
+        
+        for (int i = 0; i < copyList.Count; i += 1)
+        {
+            long currentValue = copyList[i];
+            
+            if (value <= currentValue)
+            {
+                list.Insert(i, value);
+                return Task.CompletedTask;
+            }
+
+            if (value > currentValue && i == copyList.Count - 1)
+            {
+                list.Add(value);
+                return Task.CompletedTask;
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2024.Day1;
+
+Console.WriteLine("Running Day 1...");
+Day1Logic day1 = new();
+await day1.ParseInputIntoLists();
+long totalDistance = await day1.GetTotalDistance();
+Console.WriteLine($"Total distance: {totalDistance}");
+long similarityScore = await day1.GetSimilarityScore();
+Console.WriteLine($"Similarity score: {similarityScore}");
